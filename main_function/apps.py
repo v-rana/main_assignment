80fd18c299e3c59ff278027dfd672e91332e5d0a
v-rana <63035799+v-rana@users.noreply.github.com>
2023-12-30 04:39:01 +0530

    re structuring

--- main_function/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainFunctionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main_function'
--- main_function/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from main_function.views import ngram_comparison
-
-
-urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('api/ngram-comparison/', ngram_comparison, name='ngram_comparison'),
-]
